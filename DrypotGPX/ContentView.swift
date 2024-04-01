681bf043da69fe2d2de2384174b8d1d5a5c4dd3b
drypot <drypot@gmail.com>
2024-04-01 10:07:01 +0900

    Initial Commit

---
+++ DrypotGPX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DrypotGPX
+//
+//  Created by drypot on 2024-04-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DrypotGPX/DrypotGPXApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrypotGPXApp.swift
+//  DrypotGPX
+//
+//  Created by drypot on 2024-04-01.
+//
+
+import SwiftUI
+
+@main
+struct DrypotGPXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
