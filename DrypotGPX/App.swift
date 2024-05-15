624f727b4d17c31842a5534dd1ef08d67e50d016
drypot <drypot@gmail.com>
2024-05-16 01:15:16 +0900

    add TestApp Scene for Unit Testing

---
+++ DrypotGPX/App.swift
@@ -0,0 +1,43 @@
+//
+//  DrypotGPXApp.swift
+//  DrypotGPX
+//
+//  Created by drypot on 2024-04-01.
+//
+
+import SwiftUI
+
+/*
+ How to Bypass SwiftUI App Launch During Unit Testing
+ https://qualitycoding.org/bypass-swiftui-app-launch-unit-testing/
+ */
+
+@main
+struct MainEntryPoint {
+    static func main() {
+        if isTesting() {
+            TestApp.main()
+        } else {
+            MainApp.main()
+        }
+    }
+    private static func isTesting() -> Bool {
+        return NSClassFromString("XCTestCase") != nil
+    }
+}
+
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //LocationManagerTestView()
+            MapKitViewTestView()
+        }
+    }
+}
+
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
