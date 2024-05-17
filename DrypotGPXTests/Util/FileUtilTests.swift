eb83263c464ae459632aa8cfc481c7445985b301
drypot <drypot@gmail.com>
2024-05-17 12:12:21 +0900

    update FileUtil

---
+++ DrypotGPXTests/Util/FileUtilTests.swift
@@ -0,0 +1,34 @@
+//
+//  FileUtilTests.swift
+//  DrypotGPXTests
+//
+//  Created by Kyuhyun Park on 5/15/24.
+//
+
+import XCTest
+
+final class FileUtilTests: XCTestCase {
+
+    func testEnumerateFiles() throws {
+        let baseUrl = URL(fileURLWithPath: defaultGPXFolderPath)
+        var count = 0
+        let limit = 5
+        
+        let result = enumerateFiles(at: baseUrl) { url in
+            count += 1
+            if count < limit {
+                return .success(true)
+            } else {
+                return .success(false)
+            }
+        }
+        
+        switch result {
+        case .success:
+            XCTAssertEqual(count, 5)
+        case .failure:
+            XCTFail()
+        }
+    }
+
+}
