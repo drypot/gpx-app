972b80cdac255541f9b01ab817210fe3fda2ba35
drypot <drypot@gmail.com>
2025-04-14 19:05:51 +0900

    add ErrorLogger

--- Model/Support/Error/Error.swift
+++ Model/Support/Error/Error.swift
@@ -11,3 +11,22 @@ enum LocalError: Error {
     case testing
     case general(String)
 }
+
+public class ErrorLogger {
+    private static let logger = SimpleLogger<String>()
+
+    public static func log(_ value: String) {
+        print(value)
+        logger.log(value)
+    }
+
+    public static func log(_ error: any Error) {
+        let value: String = error.localizedDescription
+        log(value)
+    }
+    
+    public static func result() -> [String] {
+        return logger.result()
+    }
+
+}
